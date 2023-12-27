378d612bf05e318a114bebbdbd7906f55189f14d
RedOasys <hooddurazi@gmail.com>
2023-12-27 10:59:15 +0300

    Fixed more in Files.

--- app/Http/Controllers/FileDisplayController.php
+++ app/Http/Controllers/FileDisplayController.php
@@ -10,15 +10,13 @@ class FileDisplayController extends Controller
 {
     public function index()
     {
-        // Fetch the recent files for the authenticated user
-        $recentFiles = FileUpload::where('user_id', Auth::id())
-            ->latest('created_at')
+        // Fetch the 10 most recent files from all users
+        $recentFiles = FileUpload::latest('created_at')
             ->take(10) // Get 10 recent files
             ->get();
 
-        // Fetch the total size of all files for the authenticated user
-        $totalSizeKB = FileUpload::where('user_id', Auth::id())
-            ->sum('file_size_kb'); // Sum of file sizes in KB
+        // Fetch the total size of all files in the database
+        $totalSizeKB = FileUpload::sum('file_size_kb'); // Sum of file sizes in KB
 
         $totalSizeGB = number_format($totalSizeKB / 1024, 2); // Convert KB to GB
 
@@ -33,16 +31,15 @@ class FileDisplayController extends Controller
     {
         $search = $request->input('search');
 
-        $recentFiles = FileUpload::where('user_id', Auth::id())
-            ->where('file_name', 'like', '%' . $search . '%') // Search for filenames containing the search string
+        // Search for filenames containing the search string among all files
+        $recentFiles = FileUpload::where('file_name', 'like', '%' . $search . '%')
             ->orderBy('created_at', 'desc')
             ->get();
 
-        // Calculate the total size in GB
+        // Calculate the total size in GB for the searched files
         $totalSizeGB = number_format($recentFiles->sum('file_size_kb') / 1024, 2);
 
         return view('files', ['recentFiles' => $recentFiles, 'totalSizeGB' => $totalSizeGB]);
-
     }
     public function fetchAllFiles(Request $request)
     {
