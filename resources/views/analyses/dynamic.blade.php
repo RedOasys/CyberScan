6b2d3deb2ffb9f2f4881b493bdf0bea265a159a2
RedOasys <hooddurazi@gmail.com>
2023-12-31 09:27:59 +0300

    test

--- resources/views/analyses/dynamic.blade.php
+++ resources/views/analyses/dynamic.blade.php
@@ -1,10 +1,7 @@
 @extends('layouts.chips.main')
 
 @section('content')
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
     <div class="row">
         <div class="col-md-12">
             <!-- Dropdown to Select Analysis -->
@@ -279,6 +276,7 @@
 
             data.forEach((screenshot, index) => {
                 const imageUrl = `/analysis/${task_id}/screenshot/${screenshot.name}`;
+                console.log("Image URL:", imageUrl); // Debugging: Log the image URL
 
                 // Create a carousel item
                 const item = document.createElement('div');
