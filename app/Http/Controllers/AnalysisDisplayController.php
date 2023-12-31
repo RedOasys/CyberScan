140e1ed484e134725cda8a6f9a6309b0eb16474a
RedOasys <hooddurazi@gmail.com>
2023-12-31 19:32:36 +0300

    Update Static.

--- app/Http/Controllers/AnalysisDisplayController.php
+++ app/Http/Controllers/AnalysisDisplayController.php
@@ -16,44 +16,35 @@ class AnalysisDisplayController extends Controller
 
         // Create the analysisData array here and pass it to the view
         $analysisData = $analyses->map(function ($analysis) {
-            $parsedData = $analysis->parsed_data;
-
-            $targetPlatform = isset($parsedData['target']['platforms'][0])
-                ? [
-                    'platform' => $parsedData['target']['platforms'][0]['platform'],
-                    'os_version' => $parsedData['target']['platforms'][0]['os_version'],
-                ]
-                : [
-                    'platform' => null,
-                    'os_version' => null,
-                ];
-
             return [
                 'id' => $analysis->id,
-                'analysis_id' => $parsedData['analysis_id'] ?? null,
-                'score' => $parsedData['score'] ?? null,
-                'category' => $parsedData['category'] ?? null,
+                'analysis_id' => $analysis->parsed_data['analysis_id'],
+                'score' => $analysis->parsed_data['score'],
+                'category' => $analysis->parsed_data['category'],
                 'target' => [
-                    'filename' => $parsedData['target']['filename'] ?? null,
-                    'orig_filename' => $parsedData['target']['orig_filename'] ?? null,
-                    'platforms' => $targetPlatform,
-                    'size' => $parsedData['target']['size'] ?? null,
-                    'filetype' => $parsedData['target']['filetype'] ?? null,
-                    'media_type' => $parsedData['target']['media_type'] ?? null,
-                    'sha256' => $parsedData['target']['sha256'] ?? null,
-                    'sha1' => $parsedData['target']['sha1'] ?? null,
-                    'md5' => $parsedData['target']['md5'] ?? null,
+                    'filename' => $analysis->parsed_data['target']['filename'],
+                    'orig_filename' => $analysis->parsed_data['target']['orig_filename'],
+                    'platforms' => [
+                        'platform' => $analysis->parsed_data['target']['platforms'][0]['platform'],
+                        'os_version' => $analysis->parsed_data['target']['platforms'][0]['os_version'],
+                    ],
+                    'size' => $analysis->parsed_data['target']['size'],
+                    'filetype' => $analysis->parsed_data['target']['filetype'],
+                    'media_type' => $analysis->parsed_data['target']['media_type'],
+                    'sha256' => $analysis->parsed_data['target']['sha256'],
+                    'sha1' => $analysis->parsed_data['target']['sha1'],
+                    'md5' => $analysis->parsed_data['target']['md5'],
                 ],
                 'static' => [
                     'pe' => [
-                        'peid_signatures' => $parsedData['static']['pe']['peid_signatures'] ?? null,
-                        'pe_imports' => $parsedData['static']['pe']['pe_imports'] ?? null,
-                        'pe_exports' => $parsedData['static']['pe']['pe_exports'] ?? null,
-                        'pe_sections' => $parsedData['static']['pe']['pe_sections'] ?? null,
-                        'pe_resources' => $parsedData['static']['pe']['pe_resources'] ?? null,
-                        'pe_versioninfo' => $parsedData['static']['pe']['pe_versioninfo'] ?? null,
-                        'pe_imphash' => $parsedData['static']['pe']['pe_imphash'] ?? null,
-                        'pe_timestamp' => $parsedData['static']['pe']['pe_timestamp'] ?? null,
+                        'peid_signatures' => $analysis->parsed_data['static']['pe']['peid_signatures'],
+                        'pe_imports' => $analysis->parsed_data['static']['pe']['pe_imports'],
+                        'pe_exports' => $analysis->parsed_data['static']['pe']['pe_exports'],
+                        'pe_sections' => $analysis->parsed_data['static']['pe']['pe_sections'],
+                        'pe_resources' => $analysis->parsed_data['static']['pe']['pe_resources'],
+                        'pe_versioninfo' => $analysis->parsed_data['static']['pe']['pe_versioninfo'],
+                        'pe_imphash' => $analysis->parsed_data['static']['pe']['pe_imphash'],
+                        'pe_timestamp' => $analysis->parsed_data['static']['pe']['pe_timestamp'],
                     ],
                 ],
             ];
